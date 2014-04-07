96d2a99966dc25a30cf1582afbd174ae495584fc
DarienneSpalding <Princessdarienne92@gmail.com>
2014-04-06 21:20:13 -0500

    Update CapitalProject.java

--- ProjectComponent/CapitalProject.java
+++ ProjectComponent/CapitalProject.java
@@ -11,6 +11,7 @@ public class CapitalProject extends Project
 {
     // instance variables - replace the example below with your own
     private Contract contract;
+    public String task;
 
     /**
      * Constructor for objects of class CapitalProject
@@ -20,6 +21,20 @@ public class CapitalProject extends Project
         super(projectName,projectDescription,sector);
         this.contract = contract;
     }
+    
+    //return location of project
+    public void getLocation( Map l)
+    {
+        return Map.(l);
+    }
+    
+    //set task 
+    public void setTask( String task)
+    {
+        this.task= task;
+    }
+    
+    
 
     
 }
