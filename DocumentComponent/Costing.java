3c7919d74337477f2d1aeee56f864928594b862b
DarienneSpalding <Princessdarienne92@gmail.com>
2014-04-06 20:47:32 -0500

    Update Costing.java
    added methods to cost

--- DocumentComponent/Costing.java
+++ DocumentComponent/Costing.java
@@ -19,6 +19,27 @@ public class Costing extends Document
     {
         super(docID,docName);
     }
-
+    //calculates cost 
+    public int calcCost(int rawMat, int miscellaneous, int salary, int machinery, int utility)
+    {
+    
+        return rawMat + miscellaneous + salary + machinery + utility;
+    }
+    
+    // generates monthly estimated cost
+    public int getEstMonthlyExp( int rawMat, int miscellaneous, int salary, int machinery, int utility)
+    {
+      int expense;
+      expense = calcCost()/12;
+      return expense;
+    }
+    
+    // returns cost of  project
+    public String createDoc()
+    {
+        String result = "";
+        result += Documents.toString();
+        result += "Cost of project : " + Cost; 
+    }
     
 }
